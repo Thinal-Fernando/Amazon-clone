1f56631b8f2498bfaaacf47f26106272848c14b5
Thinal-Fernando <r.thinal.fernando@gmail.com>
2025-05-25 12:18:04 +0530

    Maked delivery options interactive

--- Amazon-clone/scripts/checkout.js
+++ Amazon-clone/scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart, removeFromCart } from "../data/cart.js";
+import { cart, removeFromCart, updateDeliveryOption } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 import dayjs from 'https:/unpkg.com/dayjs@1.11.10/esm/index.js';
@@ -79,7 +79,7 @@ function deliveryOptionsHTML(matchingProduct, cartItem) {
     const priceString = deliveryOption.priceCents === 0 ? 'FREE' : `$${formatCurrency(deliveryOption.priceCents)} -`;
     const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
     html += `
-    <div class="delivery-option">
+    <div class="delivery-option js-delivery-option" data-product-id = "${matchingProduct.id}" data-delivery-option-id="${deliveryOption.id}">
                 <input type="radio" ${isChecked ? 'checked' : ''} class="delivery-option-input" name="delivery-option-${matchingProduct.id}">
                 <div>
                   <div class="delivery-option-date">
@@ -108,3 +108,10 @@ document.querySelectorAll('.js-delete-link')
       container.remove();
     })
   })
+
+document.querySelectorAll('.js-delivery-option').forEach((element) => {
+  element.addEventListener('click', () => {
+    const { productId, deliveryOptionId } = element.dataset;
+    updateDeliveryOption(productId, deliveryOptionId)
+  })
+})
